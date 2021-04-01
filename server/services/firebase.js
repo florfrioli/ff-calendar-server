aea0d1fa4ff64d7798ec8b88a268813045eb8d39
florfrioli <florenciaf@live.com.ar>
2021-04-01 17:27:34 -0300

    actualizo el .env con las key

--- server/services/firebase.js
+++ server/services/firebase.js
@@ -1,12 +1,13 @@
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 
+require('dotenv').config(); // Variables de entorno
 
 module.exports = firebaseConfig = {
-    apiKey: "AIzaSyCFqOFnOj_AV-nk1zd1LQH_jlPMKrtKKbI",
-    authDomain: "ff-calendar-2021.firebaseapp.com",
-    databaseURL: "https://ff-calendar-2021-default-rtdb.firebaseio.com",
-    projectId: "ff-calendar-2021",
-    storageBucket: "ff-calendar-2021.appspot.com",
-    messagingSenderId: "1097980377620",
-    appId: "1:1097980377620:web:fa1a1920490788f62ec501"
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASEURL,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGE,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
